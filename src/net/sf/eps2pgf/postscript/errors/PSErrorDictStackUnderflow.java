87c424febaeb4e593c998751018821b8e9fa5bc5
paulwag <paulwag@c58e4fe5-e728-0410-8ce3-8a4ff3be88f8>
2007-03-18 14:04:57 +0000

    Added: PostScript operators <<, >> and idiv
    git-svn-id: https://svn.code.sf.net/p/eps2pgf/code/trunk@39 c58e4fe5-e728-0410-8ce3-8a4ff3be88f8

---
+++ src/net/sf/eps2pgf/postscript/errors/PSErrorDictStackUnderflow.java
@@ -0,0 +1,34 @@
+/*
+ * PSErrorDictStackUnderflow.java
+ *
+ * This file is part of Eps2pgf.
+ *
+ * Copyright (C) 2007 Paul Wagenaars <pwagenaars@fastmail.fm>
+ *
+ * Eps2pgf is free software; you can redistribute it and/or modify it under
+ * the terms of version 2 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * Eps2pgf is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package net.sf.eps2pgf.postscript.errors;
+
+/**
+ *
+ * @author Paul Wagenaars
+ */
+public class PSErrorDictStackUnderflow extends PSError {
+    
+    /** Creates a new instance of PSErrorDictStackUnderflow */
+    public PSErrorDictStackUnderflow() {
+    }
+    
+}
