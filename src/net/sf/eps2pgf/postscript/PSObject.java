809079d4beb0c33034b675b6f93be3c3b43f61bd
paulwag <paulwag@c58e4fe5-e728-0410-8ce3-8a4ff3be88f8>
2007-04-19 19:48:23 +0000

    Fixed: cleaned up PSObject base class (functionality unchanged)
    git-svn-id: https://svn.code.sf.net/p/eps2pgf/code/trunk@88 c58e4fe5-e728-0410-8ce3-8a4ff3be88f8

--- src/net/sf/eps2pgf/postscript/PSObject.java
+++ src/net/sf/eps2pgf/postscript/PSObject.java
@@ -32,43 +32,7 @@ import net.sf.eps2pgf.postscript.errors.*;
  * @author Paul Wagenaars
  */
 public class PSObject implements Cloneable {
-    /**
-     * Checks whether the supplied string is of this type.
-     * @param str String to be checked.
-     * @return Return true when the string of the objects type.
-     */
-    public static boolean isType(String str) {
-        return false;
-    }
 
-    /**
-     * Executes this object in the supplied interpreter
-     * @param interp Interpreter in which this object is executed.
-     * @throws java.lang.Exception An error occured during the execution of this object.
-     */
-    public void execute(Interpreter interp) throws Exception {
-        interp.opStack.push(this);
-    }
-    
-    /**
-     * Process this object in the supplied interpreter. This is the way
-     * objects from the operand stack are processed.
-     * @param interp Interpreter in which this object is processed.
-     * @throws java.lang.Exception An error occured during the execution of this object.
-     */
-    public void process(Interpreter interp) throws Exception {
-        interp.opStack.push(this);
-    }
-    
-    /**
-     * Return PostScript text representation of this object. See the
-     * PostScript manual under the == operator
-     * @return Text representation of this object.
-     */
-    public String isis() {
-        return this.getClass().getName();
-    }
-    
     /**
      * Returns the absolute value of this object, if possible
      * @return Absolute value of this object
@@ -89,6 +53,17 @@ public class PSObject implements Cloneable {
     }
     
     /**
+     * Implements bind operator for this object. For most object this wil
+     * be the same object, without any change.
+     * @param interp Interpreter in which this object will be executed.
+     * @return Return this object after binding
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck This object type does not support bind.
+     */
+    public PSObject bind(Interpreter interp) throws PSErrorTypeCheck {
+        return this;
+    }
+    
+    /**
      * Return this value rounded upwards
      * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Object is not numeric
      * @return Value of this object rounded upwards
@@ -98,14 +73,36 @@ public class PSObject implements Cloneable {
     }
     
     /**
+     * Creates a copy of this object.
+     * @throws java.lang.CloneNotSupportedException This object does not suppert cloneing.
+     * @return Deep copy of this object
+     */
+    public PSObject clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+    
+    /**
      * PostScript operator copy. Copies values from obj1 to this object.
      * @param obj1 Copy values from obj1
      * @return Returns subsequence of this object
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorRangeCheck Subarray/substring longer that original array/string
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Invalid or incompatible object type(s) for copy operator
      */
     public PSObject copy(PSObject obj1) throws PSErrorRangeCheck, PSErrorTypeCheck {
         throw new PSErrorTypeCheck();
     }
     
+    /**
+     * Copies values from another object to this object, if possible.
+     * @param obj Object from which the values must the copied
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorRangeCheck The supplied object does not have the correct number of elements.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck The supplied object can not be converted to the same type as this
+     * object.
+     */
+    public void copyValuesFrom(PSObject obj) throws PSErrorRangeCheck, PSErrorTypeCheck {
+        throw new PSErrorTypeCheck();
+    }
+
     /**
      * Produce a text representation of this object (see PostScript
      * operator 'cvs' for more info)
@@ -113,6 +110,25 @@ public class PSObject implements Cloneable {
      */
     public String cvs() {
         return "--nostringval--";
+    }
+    
+    /**
+     * Compare this object with another object and return true if they are equal.
+     * See PostScript manual on what's equal and what's not.
+     * @param obj Object to compare this object with
+     * @return True if objects are equal, false otherwise
+     */
+    public boolean eq(PSObject obj) {
+        return (this == obj);
+    }
+    
+    /**
+     * Executes this object in the supplied interpreter
+     * @param interp Interpreter in which this object is executed.
+     * @throws java.lang.Exception An error occured during the execution of this object.
+     */
+    public void execute(Interpreter interp) throws Exception {
+        interp.opStack.push(this);
     }
     
     /**
@@ -127,6 +143,11 @@ public class PSObject implements Cloneable {
     /**
      * PostScript operator: get
      * Gets a single element from this object.
+     * @param index Index/key of object to get
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Can not 'get' from this object or invalid index/key
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorRangeCheck Index out of bounds
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorUndefined Unknown key
+     * @return Requested object
      */
     public PSObject get(PSObject index) throws PSErrorTypeCheck,
             PSErrorRangeCheck, PSErrorUndefined {
@@ -154,11 +175,38 @@ public class PSObject implements Cloneable {
      *         in a single item. For most object types this is 1, but for
      *         dictionaries this is 2. All consecutive items (index 1 and
      *         up) are the object's items.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck This object does not have a list of items
      */
     public List<PSObject> getItemList() throws PSErrorTypeCheck {
         throw new PSErrorTypeCheck();
     }
     
+    /**
+     * Checks whether this object is executable
+     * @return Returns true if this object is executable
+     */
+    public boolean isExecutable() {
+        return false;
+    }
+    
+    /**
+     * Return PostScript text representation of this object. See the
+     * PostScript manual under the == operator
+     * @return Text representation of this object.
+     */
+    public String isis() {
+        return this.getClass().getName();
+    }
+    
+    /**
+     * Checks whether the supplied string is of this type.
+     * @param str String to be checked.
+     * @return Return true when the string of the objects type.
+     */
+    public static boolean isType(String str) {
+        return false;
+    }
+
     /**
      * Implements PostScript operate: length
      * @return Length of this object
@@ -188,9 +236,21 @@ public class PSObject implements Cloneable {
     }
     
     /**
+     * Process this object in the supplied interpreter. This is the way
+     * objects from the operand stack are processed.
+     * @param interp Interpreter in which this object is processed.
+     * @throws java.lang.Exception An error occured during the execution of this object.
+     */
+    public void process(Interpreter interp) throws Exception {
+        interp.opStack.push(this);
+    }
+    
+    /**
      * PostScript operator put. Replace a single value in this object.
      * @param index Index or key for new value
      * @param value New value
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorRangeCheck Invalid index or key
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Can not 'put' anything in this object type
      */
     public void put(PSObject index, PSObject value) throws PSErrorRangeCheck,
             PSErrorTypeCheck {
@@ -201,6 +261,8 @@ public class PSObject implements Cloneable {
      * PostScript operator putinterval
      * @param index Start index of subsequence
      * @param obj Subsequence
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Can not 'putinterval' anything in this object type
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorRangeCheck Index or (index+length) out of bounds
      */
     public void putinterval(int index, PSObject obj) throws PSErrorTypeCheck, PSErrorRangeCheck {
         throw new PSErrorTypeCheck();
@@ -226,15 +288,6 @@ public class PSObject implements Cloneable {
     }
     
     /**
-     * Return this value towards zero
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Object is not numeric
-     * @return Value of this object towards zero
-     */
-    public PSObject truncate() throws PSErrorTypeCheck {
-        throw new PSErrorTypeCheck();
-    }
-    
-    /**
      * Convert this object to an array, if possible.
      * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Can't convert this object to an array.
      * @return Array representation of this object.
@@ -244,11 +297,80 @@ public class PSObject implements Cloneable {
     }
     
     /**
+     * Convert this object to a boolean, if possible.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a boolean
+     * @return Boolean representation of this object
+     */
+    public boolean toBool() throws PSErrorTypeCheck {
+        throw new PSErrorTypeCheck();
+    }
+    
+    /**
+     * Convert this object to a dictionary, if possible.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a dictionary
+     * @return Dictionary representation of this object.
+     */
+    public PSObjectDict toDict() throws PSErrorTypeCheck {
+        throw new PSErrorTypeCheck();
+    }
+    
+    /**
+     * Convert this object to dictionary key, if possible.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a dict key
+     * @return Dictionary key that represents this object
+     */
+    public String toDictKey() throws PSErrorTypeCheck {
+        throw new PSErrorTypeCheck();
+    }
+    
+    /**
+     * Convert this object to a font dictionary, if possible.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a dictionary
+     * @return Dictionary representation of this object.
+     */
+    public PSObjectFont toFont() throws PSErrorTypeCheck {
+        throw new PSErrorTypeCheck();
+    }
+    
+    /**
+     * Convert this object to a FontBox FontMetric object, if possible
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a FontMetric
+     * @return FontMetric object
+     */
+    public FontMetric toFontMetric() throws PSErrorTypeCheck {
+        throw new PSErrorTypeCheck();
+    }
+    
+    /**
      * Convert this object to an integer, if possible.
      * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object to an integer
      * @return Integer representation of this object
      */
     public int toInt() throws PSErrorTypeCheck {
+        throw new PSErrorTypeCheck();
+    }
+    
+    /**
+     * Convert this object to a literal object
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorUnimplemented Converting this object type to literal is not yet supported
+     * @return This object converted to a literal object
+     */
+    public PSObject toLiteral() throws PSErrorUnimplemented {
+        if (isExecutable()) {
+            throw new PSErrorUnimplemented("Making this object literal");
+        } else {
+            return this;
+        }
+    }
+    
+    /**
+     * Convert this object to a matrix, if possible.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorRangeCheck This object does not have the correct number of elements. A matrix
+     * should have six elements.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a matrix
+     * @return Matrix representation of this object
+     */
+    public PSObjectMatrix toMatrix() throws PSErrorRangeCheck, PSErrorTypeCheck {
         throw new PSErrorTypeCheck();
     }
     
@@ -290,51 +412,6 @@ public class PSObject implements Cloneable {
     }
     
     /**
-     * Convert this object to dictionary key, if possible.
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a dict key
-     * @return Dictionary key that represents this object
-     */
-    public String toDictKey() throws PSErrorTypeCheck {
-        throw new PSErrorTypeCheck();
-    }
-    
-    /**
-     * Convert this object to a boolean, if possible.
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a boolean
-     * @return Boolean representation of this object
-     */
-    public boolean toBool() throws PSErrorTypeCheck {
-        throw new PSErrorTypeCheck();
-    }
-    
-    /**
-     * Convert this object to a dictionary, if possible.
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a dictionary
-     * @return Dictionary representation of this object.
-     */
-    public PSObjectDict toDict() throws PSErrorTypeCheck {
-        throw new PSErrorTypeCheck();
-    }
-    
-    /**
-     * Convert this object to a font dictionary, if possible.
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a dictionary
-     * @return Dictionary representation of this object.
-     */
-    public PSObjectFont toFont() throws PSErrorTypeCheck {
-        throw new PSErrorTypeCheck();
-    }
-    
-    /**
-     * Convert this object to a FontBox FontMetric object, if possible
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a FontMetric
-     * @return FontMetric object
-     */
-    public FontMetric toFontMetric() throws PSErrorTypeCheck {
-        throw new PSErrorTypeCheck();
-    }
-    
-    /**
      * Convert this object to a procedure object, if possible.
      * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a procedure.
      * @return Procedure representation of this object
@@ -353,17 +430,6 @@ public class PSObject implements Cloneable {
     }
     
     /**
-     * Convert this object to a matrix, if possible.
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorRangeCheck This object does not have the correct number of elements. A matrix
-     * should have six elements.
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unable to convert this object type to a matrix
-     * @return Matrix representation of this object
-     */
-    public PSObjectMatrix toMatrix() throws PSErrorRangeCheck, PSErrorTypeCheck {
-        throw new PSErrorTypeCheck();
-    }
-    
-    /**
      * Convert this object to a real number, if possible.
      * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Unbale to convert this object to a real number
      * @return Floating-point number representation of this object
@@ -373,66 +439,12 @@ public class PSObject implements Cloneable {
     }
     
     /**
-     * Copies values from another object to this object, if possible.
-     * @param obj Object from which the values must the copied
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorRangeCheck The supplied object does not have the correct number of elements.
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck The supplied object can not be converted to the same type as this
-     * object.
+     * Return this value towards zero
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Object is not numeric
+     * @return Value of this object towards zero
      */
-    public void copyValuesFrom(PSObject obj) throws PSErrorRangeCheck, PSErrorTypeCheck {
+    public PSObject truncate() throws PSErrorTypeCheck {
         throw new PSErrorTypeCheck();
-    }
-
-    
-    /**
-     * Creates a copy of this object.
-     * @throws java.lang.CloneNotSupportedException This object does not suppert cloneing.
-     * @return Deep copy of this object
-     */
-    public PSObject clone() throws CloneNotSupportedException {
-        throw new CloneNotSupportedException();
-    }
-    
-    /**
-     * Compare this object with another object and return true if they are equal.
-     * See PostScript manual on what's equal and what's not.
-     * @param obj Object to compare this object with
-     * @return True if objects are equal, false otherwise
-     */
-    public boolean eq(PSObject obj) {
-        return (this == obj);
-    }
-    
-    /**
-     * Implements bind operator for this object. For most object this wil
-     * be the same object, without any change.
-     * @param interp Interpreter in which this object will be executed.
-     * @return Return this object after binding
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck This object type does not support bind.
-     */
-    public PSObject bind(Interpreter interp) throws PSErrorTypeCheck {
-        return this;
-    }
-    
-    /**
-     * Checks whether this object is executable
-     * @return Returns true if this object is executable
-     */
-    public boolean isExecutable() {
-        return false;
-    }
-    
-    /**
-     * Convert this object to a literal object
-     * @throws net.sf.eps2pgf.postscript.errors.PSErrorUnimplemented Converting this object type to literal is not yet supported
-     * @return This object converted to a literal object
-     */
-    public PSObject toLiteral() throws PSErrorUnimplemented {
-        if (isExecutable()) {
-            throw new PSErrorUnimplemented("Making this object literal");
-        } else {
-            return this;
-        }
     }
     
     /**
