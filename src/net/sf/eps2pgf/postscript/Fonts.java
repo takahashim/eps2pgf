7dceed43850d1a13a41afb30c4fff590bd24ef71
paulwag <paulwag@c58e4fe5-e728-0410-8ce3-8a4ff3be88f8>
2007-07-29 18:16:12 +0000

    Added: Font substitution list
    git-svn-id: https://svn.code.sf.net/p/eps2pgf/code/trunk@200 c58e4fe5-e728-0410-8ce3-8a4ff3be88f8

--- src/net/sf/eps2pgf/postscript/Fonts.java
+++ src/net/sf/eps2pgf/postscript/Fonts.java
@@ -37,10 +37,13 @@ public class Fonts {
     
     File resourceDir;
     
+    Properties fontSubstitutions;
+    
     Logger log = Logger.getLogger("global");
     
     /**
      * Create a new Fonts instance
+     * @throws java.io.FileNotFoundException Unable to find resource dir
      */
     public Fonts() throws FileNotFoundException {
         // Try to find the resource dir
@@ -54,7 +57,7 @@ public class Fonts {
             // It just checks for a "resources" directory with two known subdirectories
             resourceDir = new File(classPath, "resources");
             if (resourceDir.exists()) {
-            File afmDir = new File(resourceDir, "afm");
+                File afmDir = new File(resourceDir, "afm");
                 if (afmDir.exists()) {
                     File fontDescDir = new File(resourceDir, "fontdescriptions");
                     if (fontDescDir.exists()) {
@@ -68,10 +71,19 @@ public class Fonts {
         if (resourceDir == null) {
             throw new FileNotFoundException("Unable to find resource dir.");
         }
+        
+        fontSubstitutions = loadFontSubstitutions(new File(resourceDir, "fontSubstitution.xml"));
     }
     
     /**
      * Search a font and return it's corresponding font dictionary.
+     * @param fontNameObj PostScript object defining the font. The toDictKey method is used to get
+     * the font name from the object.
+     * @return Requested font
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorTypeCheck Supplied opject is not the correct type
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorInvalidFont Unable to find the requested font.
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorInvalidAccess No access to a required resource.
+     * @throws net.sf.eps2pgf.ProgramError This should never happen. This error indicates a bug in Eps2pgf.
      */
     public PSObjectFont findFont(PSObject fontNameObj) throws PSErrorTypeCheck, 
             PSErrorInvalidFont, PSErrorInvalidAccess, ProgramError {
@@ -86,6 +98,12 @@ public class Fonts {
      * @throws ProgramError Unable to load the requested font and the default font
      */
     public PSObjectFont findFont(String fontName) throws ProgramError {
+        String subFontName = findSubstitutionFont(fontName);
+        if (subFontName != null) {
+            log.info("Substituting font " + subFontName + " for " + fontName);
+            fontName = subFontName;
+        }
+        
         // First we search whether this font has already been loaded
         if (fonts.containsKey(fontName)) {
             return fonts.get(fontName);
@@ -106,9 +124,26 @@ public class Fonts {
     }
     
     /**
+     * Search the font substitution list for a font.
+     * @return If the requested font is found in the substitution list the
+     *         substitution font is returned. If the requested font is not
+     *         found, the requested font itself is returned.
+     * @param fontName Name of the font for which a substitute is searched
+     */
+    public String findSubstitutionFont(String fontName) {
+        String subFont = fontSubstitutions.getProperty(fontName);
+        if (subFont == null) {
+            return null;
+        } else {
+            return subFont;
+        }
+    }
+    
+    /**
      * Load a font from the resource directory.
-     * @param fontName Name of the font
      * @return Font dictionary of the loaded font
+     * @param fontName Name of the font
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorInvalidFont Unable to find requested font.
      */
     public PSObjectFont loadFont(String fontName) throws PSErrorInvalidFont {
         log.info("Loading " + fontName + " font from " + resourceDir);
@@ -126,6 +161,7 @@ public class Fonts {
      * @return Defined font
      * @param key Key to associate the font with
      * @param font Font to define
+     * @throws net.sf.eps2pgf.postscript.errors.PSErrorInvalidAccess No access to a required resource.
      * @throws net.sf.eps2pgf.postscript.errors.PSErrorUnimplemented Part of this method is not implemented
      * @throws PSErrorTypeCheck Unable to use the key as dictionary key
      */
@@ -140,5 +176,25 @@ public class Fonts {
         return font;
     }
     
+    /**
+     * Load list with font substitutions
+     * @param fontSubFile File from which the substitution list will be loaded
+     * @return List with font substitution. Returns null if the list could not
+     *         be loaded.
+     */
+    public Properties loadFontSubstitutions(File fontSubFile) {
+        Properties fontSubList;
+        try {
+            FileInputStream in = new FileInputStream(fontSubFile);
+            fontSubList = new Properties();
+            fontSubList.loadFromXML(in);
+            in.close();
+        } catch (FileNotFoundException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+        return fontSubList;
+    }
 
 }
