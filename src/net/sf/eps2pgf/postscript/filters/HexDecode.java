f58543bf3f1917361d87afcc5db83b2b0e0b3d5d
paulwag <paul@wagenaars.org>
2007-09-08 16:52:56 +0000

    Added: implemented all other methods required for correct operation
    git-svn-id: https://svn.code.sf.net/p/eps2pgf/code/trunk@241 c58e4fe5-e728-0410-8ce3-8a4ff3be88f8

--- src/net/sf/eps2pgf/postscript/filters/HexDecode.java
+++ src/net/sf/eps2pgf/postscript/filters/HexDecode.java
@@ -31,9 +31,67 @@ import java.io.IOException;
 public class HexDecode extends InputStream {
     private InputStream in;
     
-    /** Creates a new instance of HexDecode */
+    /**
+     * Creates a new instance of HexDecode
+     * @param in <code>InputStream</code> from which hex data will be read.
+     */
     public HexDecode(InputStream in) {
         this.in = in;
+    }
+    
+    /**
+     * Returns the number of bytes that can be read (or skipped over) from this
+     * input stream without blocking by the next caller of a method for this input
+     * stream.
+     * @throws java.io.IOException An I/O error occurred.
+     * @return The number of bytes that can be read from this input stream
+     * without blocking.
+     */
+    public int available() throws IOException {
+        int n = in.available();
+        n = (n + 1) / 2;
+        return n;
+    }
+    
+    /**
+     * Closes this input stream and releases any system resources associated with
+     * the stream.
+     * @throws java.io.IOException An I/O error occurred.
+     */
+    public void close() throws IOException {
+        in.close();
+    }
+    
+    /**
+     * Marks the current position in this input stream. A subsequent call to the
+     * reset method repositions this stream at the last marked position so that
+     * subsequent reads re-read the same bytes.
+     * 
+     * The readlimit arguments tells this input stream to allow that many bytes to
+     * be read before the mark position gets invalidated.
+     * 
+     * The general contract of mark is that, if the method markSupported returns
+     * true, the stream somehow remembers all the bytes read after the call to mark
+     * and stands ready to supply those same bytes again if and whenever the
+     * method reset is called. However, the stream is not required to remember any
+     * data at all if more than readlimit bytes are read from the stream before
+     * reset is called.
+     * @param readlimit The maximum limit of bytes that can be read before the mark
+     * position becomes invalid.
+     */
+    public void mark(int readlimit) {
+        in.mark(readlimit);
+    }
+    
+    /**
+     * Tests if this input stream supports the mark and reset methods. Whether or
+     * not mark and reset are supported is an invariant property of a particular
+     * input stream instance.
+     * @return <code>true</code> if this stream instance supports the mark and
+     * reset methods; <code>false</code> otherwise.
+     */
+    public boolean markSupported() {
+        return in.markSupported();
     }
     
     /**
@@ -42,6 +100,8 @@ public class HexDecode extends InputStream {
      * the end of the stream has been reached, the value -1 is returned. This
      * method blocks until input data is available, the end of the stream is
      * detected, or an exception is thrown.
+     * @throws java.io.IOException An I/O exception occurred.
+     * @return The next byte of data, or -1 if the end of the stream is reached.
      */
     public int read() throws IOException {
         int c1 = readNextHexChar();
@@ -85,4 +145,27 @@ public class HexDecode extends InputStream {
         return c;
     }
     
+    /**
+     * Repositions this stream to the position at the time the mark method was last
+     * called on this input stream.
+     * @throws java.io.IOException An I/O exception occurred.
+     */
+    public void reset() throws IOException {
+        in.reset();
+    }
+    
+    /**
+     * Skips over and discards n bytes of data from this input stream. The skip
+     * method may, for a variety of reasons, end up skipping over some smaller
+     * number of bytes, possibly 0. This may result from any of a number of
+     * conditions; reaching end of file before n bytes have been skipped is only one
+     * possibility. The actual number of bytes skipped is returned. If n is
+     * negative, no bytes are skipped.
+     * @param n The number of bytes to be skipped.
+     * @throws java.io.IOException An I/O exception occurred.
+     * @return The actual number of bytes skipped.
+     */
+    public long skip(long n) throws IOException {
+        return in.skip(2 * n);
+    }
 }
