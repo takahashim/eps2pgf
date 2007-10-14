2494a0a7865e53742b6ba6332add96f1aec057b3
paulwag <paulwag@c58e4fe5-e728-0410-8ce3-8a4ff3be88f8>
2007-10-14 18:33:48 +0000

    Changed: new email address
    git-svn-id: https://svn.code.sf.net/p/eps2pgf/code/trunk@264 c58e4fe5-e728-0410-8ce3-8a4ff3be88f8

--- src/net/sf/eps2pgf/postscript/PSObjectIterator.java
+++ src/net/sf/eps2pgf/postscript/PSObjectIterator.java
@@ -3,7 +3,7 @@
  *
  * This file is part of Eps2pgf.
  *
- * Copyright 2007 Paul Wagenaars <pwagenaars@fastmail.fm>
+ * Copyright 2007 Paul Wagenaars <paul@wagenaars.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
