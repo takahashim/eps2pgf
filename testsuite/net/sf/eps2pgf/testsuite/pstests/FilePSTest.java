8b50e3f91646c82498bd3ff4eca882873f866667
paulwag <paul@wagenaars.org>
2008-12-31 17:29:53 +0000

    Changed: tests now check stack size
    git-svn-id: https://svn.code.sf.net/p/eps2pgf/code/trunk@483 c58e4fe5-e728-0410-8ce3-8a4ff3be88f8

--- testsuite/net/sf/eps2pgf/testsuite/pstests/FilePSTest.java
+++ testsuite/net/sf/eps2pgf/testsuite/pstests/FilePSTest.java
@@ -59,30 +59,30 @@ public class FilePSTest {
     
     /** Test. @throws Exception the exception */
     @Test
-    public void fileTest1_Currentfile() throws Exception {
+    public void currentfile1() throws Exception {
         String cmd = "currentfile type /filetype eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void fileTest2_Readstring() throws Exception {
+    public void readstring1() throws Exception {
         String cmd = "currentfile (abc) readstring  123 pop exch ( 12) eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void fileTest3_Readstring() throws Exception {
+    public void readstring2() throws Exception {
         String cmd = "{currentfile () readstring} stopped"
             + " count 1 roll"
             + " 1 1 count 3 sub {pop pop} for";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void fileTest4() throws Exception {
+    public void decodeFilters1() throws Exception {
         String cmd = "currentfile /ASCII85Decode filter /RunLengthDecode filter"
             + " 100 string readstring\n"
             + "@fQ`D'c\\GJ0fVB&!!`uK*$Zpf3\"?/n#7_Ig,:YD_%L2t=%M]s..NB05#64u='c"
@@ -96,83 +96,83 @@ public class FilePSTest {
             + "\\007\\000\\007\\016\\0251*#\\034\\025\\016\\007\\000\\007\\0168"
             + "1*#\\034\\025\\016\\007\\000\\007?81*#\\034\\025\\016\\007\\000)"
             + "\neq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
     
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest5_Readhexstring() throws Exception {
+    public void readhexstring1() throws Exception {
         String cmd = "currentfile 4 string readhexstring 1aBf90f6 exch"
             + " (\032\277\220\366) eq\n";
         cmd += "currentfile 6 string readhexstring 123456>notexecutedFexch"
             + " (\0224V\356\316\337) eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 4));
     }
 
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest6_SubFileDecode() throws Exception {
+    public void subFileDecode1() throws Exception {
         String cmd = "currentfile 0 (TheEnd) /SubFileDecode filter 99 string"
             + " readstring blablaTheEndfalse eq 2 1 roll (blabla) eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
 
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest7_SubFileDecode() throws Exception {
+    public void subFileDecode2() throws Exception {
         String cmd = "currentfile << /Foo (bar) >> 1 (TheEnd) /SubFileDecode"
             + " filter 99 string"
             + " readstring blablaTheEndfalse eq 2 1 roll (blablaTheEnd) eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
 
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest8_SubFileDecode() throws Exception {
+    public void subFileDecode3() throws Exception {
         String cmd = "currentfile 6 () /SubFileDecode filter 99 string"
             + " readstring blablafalse eq 2 1 roll (blabla) eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
 
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest9_SubFileDecode() throws Exception {
+    public void subFileDecode4() throws Exception {
         String cmd = "currentfile 2 (TheEnd) /SubFileDecode filter 99 string"
             + " readstring blaTheEndblaTheEnd false eq 2 1 roll"
             + " (blaTheEndblaTheEnd) eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
 
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest10_SubFileDecode() throws Exception {
+    public void subFileDecode5() throws Exception {
         String cmd = "currentfile << /EODCount 2 /EODString (TheEnd) >>"
             + " /SubFileDecode filter 99 string"
             + " readstring blaTheEndblaTheEnd false eq 2 1 roll"
             + " (blaTheEndblaTheEnd) eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
 
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest11_Status() throws Exception {
+    public void status1() throws Exception {
         String cmd = "currentfile /ASCIIHexDecode filter dup dup status 3 1"
             + " roll closefile status false eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
     
     
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest12_Flushfile() throws Exception {
+    public void flushfile1() throws Exception {
         String cmd = "currentfile 0 (%%End) /SubFileDecode filter dup flushfile"
             + " %%End status false eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
     
     /** Test. @throws Exception the exception. */
     @Test
-    public void fileTest13() throws Exception {
+    public void misc1() throws Exception {
         String cmd = "currentfile 5 string readline k\n"
             + "exch (k) eq\n"
             + "currentfile 10 string readline "
@@ -181,7 +181,7 @@ public class FilePSTest {
             + "currentfile 10 string dup 3 1 roll readline\n"
             + "next line\n"
             + "3 1 roll pop (next line\000) eq\n";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 6));
     }
     
 
