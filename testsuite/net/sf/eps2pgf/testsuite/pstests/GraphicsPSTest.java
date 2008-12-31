f8aa8574d16c6f6c9642e59eb63921f60e8b2ca2
paulwag <paul@wagenaars.org>
2008-12-31 21:18:37 +0000

    Changed: tests now check stack size
    git-svn-id: https://svn.code.sf.net/p/eps2pgf/code/trunk@487 c58e4fe5-e728-0410-8ce3-8a4ff3be88f8

--- testsuite/net/sf/eps2pgf/testsuite/pstests/GraphicsPSTest.java
+++ testsuite/net/sf/eps2pgf/testsuite/pstests/GraphicsPSTest.java
@@ -59,227 +59,227 @@ public class GraphicsPSTest {
     
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest1Currentcolorspace() throws Exception {
+    public void currentcolorspace1() throws Exception {
         String cmd = "currentcolorspace 0 get /DeviceGray eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest2Setcolorspace() throws Exception {
+    public void setcolorspace1() throws Exception {
         String cmd = "/DeviceRGB setcolorspace currentcolorspace 0 get"
             + " /DeviceRGB eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest3Setcolorspace() throws Exception {
+    public void setcolorspace2() throws Exception {
         String cmd = "[/Indexed /DeviceRGB 2 <FF0000 00FF00 0000FF>]"
             + " setcolorspace 1.0 setcolor currentrgbcolor 0.0 eq 3 1 roll 1.0"
             + " eq 3 1 roll 0.0 eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest4Setcolorspace() throws Exception {
+    public void setcolorspace3() throws Exception {
         String cmd = "[/Indexed /DeviceRGB 3 <012345 6789AB CDEF01 234567>]"
             + " setcolorspace"
             + " currentcolorspace 3 get <012345 6789AB CDEF01 234567> eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest4Currentcolor() throws Exception {
+    public void currentcolor1() throws Exception {
         String cmd = "currentcolor 0 eq count 1 eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest5Setcolor() throws Exception {
+    public void setcolor1() throws Exception {
         String cmd = "0.5 setcolor currentcolor 0.5 eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest6Setcolor() throws Exception {
+    public void setcolor2() throws Exception {
         String cmd = "0.5 setcolor /DeviceRGB setcolorspace currentcolor 0 eq"
             + " 3 1 roll 0 eq 3 1 roll 0 eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest7Setcolor() throws Exception {
+    public void setcolor3() throws Exception {
         String cmd = "/DeviceRGB setcolorspace 0.1 0.2 0.3 setcolor"
             + " currentcolor 0.3 eq 3 1 roll 0.2 eq 3 1 roll 0.1 eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest8Setgray() throws Exception {
+    public void setgray1() throws Exception {
         String cmd = "0.4 setgray currentcolorspace 0 get /DeviceGray eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest9Setgray() throws Exception {
+    public void setgray2() throws Exception {
         String cmd = "0.4 setgray currentcolor 0.4 eq count 1 eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest10Sethsbcolor() throws Exception {
+    public void sethsbcolor1() throws Exception {
         String cmd = "0.123 0.456 0.789 sethsbcolor currentcolorspace 0 get"
             + " /DeviceRGB eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest11Sethsbcolor() throws Exception {
+    public void sethsbcolor2() throws Exception {
         String cmd = "0.123 0.456 0.789 sethsbcolor currentcolor 0.4292 div dup"
             + " 1 lt {1 exch div} if 1.001 le  3 1 roll 0.6947 div dup 1 lt"
             + " {1 exch div} if 1.001 le  3 1 roll 0.789 div dup 1 lt {1 exch"
             + " div} if 1.001 le";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest12Setrgbcolor() throws Exception {
+    public void setrgbcolor1() throws Exception {
         String cmd = "0.123 0.456 0.789 setrgbcolor currentcolorspace 0 get"
             + " /DeviceRGB eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest13Setrgbcolor() throws Exception {
+    public void setrgbcolor2() throws Exception {
         String cmd = "0.123 0.456 0.789 setrgbcolor currentcolor 0.789 div dup"
             + " 1 lt {1 exch div} if 1.001 le  3 1 roll 0.456 div dup 1 lt {1"
             + " exch div} if 1.001 le  3 1 roll 0.123 div dup 1 lt {1 exch div}"
             + " if 1.001 le";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest14Setcmykcolor() throws Exception {
+    public void setcmykcolor1() throws Exception {
         String cmd = "0.12 0.34 0.56 0.78 setcmykcolor currentcolorspace 0 get"
             + " /DeviceCMYK eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest15Setcmykcolor() throws Exception {
+    public void setcmykcolor2() throws Exception {
         String cmd = "0.12 0.34 0.56 0.78 setcmykcolor currentcolor 0.78 div"
             + " dup 1 lt {1 exch div} if 1.001 le  4 1 roll 0.56 div dup 1 lt"
             + " {1 exch div} if 1.001 le  4 1 roll 0.34 div dup 1 lt"
             + " {1 exch div} if 1.001 le  4 1 roll 0.12 div dup 1 lt"
             + " {1 exch div} if 1.001 le";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 4));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest16Currentgray() throws Exception {
+    public void currentgray1() throws Exception {
         String cmd = "0.25 setgray currentgray 0.25 sub abs 0.0001 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest17Currentgray() throws Exception {
+    public void currentgray2() throws Exception {
         String cmd = "0.25 1 0.5 setrgbcolor currentgray 0.72 sub abs 0.0001"
             + " lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest18Currentgray() throws Exception {
+    public void currentgray3() throws Exception {
         String cmd = "0.1 0.2 0.3 0.4 setcmykcolor currentgray 0.419 sub abs"
             + " 0.0001 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 1));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest19Currenthsbcolor() throws Exception {
+    public void currenthsbcolor1() throws Exception {
         String cmd = "0.25 setgray currenthsbcolor 0.25 sub abs 0.0001 lt"
             + " 3 1 roll abs 0.0001 lt  3 1 roll abs 0.0001 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest20Currenthsbcolor() throws Exception {
+    public void currenthsbcolor2() throws Exception {
         String cmd = "0.25 1 0.5 setrgbcolor currenthsbcolor  1.0 div dup 1 lt"
             + " {1 exch div} if 1.001 le  3 1 roll 0.75 div dup 1 lt"
             + " {1 exch div} if 1.001 le  3 1 roll 0.3889 div dup 1 lt"
             + " {1 exch div} if 1.001 le";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest21Currenthsbcolor() throws Exception {
+    public void currenthsbcolor3() throws Exception {
         String cmd = "0.1 0.2 0.3 0.4 setcmykcolor currenthsbcolor 0.5 sub abs"
             + " 0.0001 lt  3 1 roll 0.4 sub abs 0.0001 lt  3 1 roll 0.08333 sub"
             + " abs 0.0001 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest22Currentrgbcolor() throws Exception {
+    public void currentrgbcolor1() throws Exception {
         String cmd = "0.25 setgray currentrgbcolor  0.25 sub abs 0.0001 lt"
             + " 3 1 roll 0.25 sub abs 0.0001 lt"
             + " 3 1 roll 0.25 sub abs 0.0001 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest23Currentrgbcolor() throws Exception {
+    public void currentrgbcolor2() throws Exception {
         String cmd = "0.25 1 0.5 setrgbcolor currentrgbcolor  0.5 div dup 1 lt"
             + " {1 exch div} if 1.001 le  3 1 roll 1.0 div dup 1 lt"
             + " {1 exch div} if 1.001 le  3 1 roll 0.25 div dup 1 lt"
             + " {1 exch div} if 1.001 le";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest24Currentrgbcolor() throws Exception {
+    public void currentrgbcolor3() throws Exception {
         String cmd = "0.1 0.2 0.3 0.4 setcmykcolor currentrgbcolor"
             + " 0.3 sub abs 1e-4 lt"
             + " 3 1 roll 0.4 sub abs 1e-4 lt"
             + " 3 1 roll 0.5 sub abs 1e-4 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 3));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest25Currentcmykcolor() throws Exception {
+    public void currentcmykcolor1() throws Exception {
         String cmd = "0.25 setgray currentcmykcolor  0.75 sub abs 0.0001 lt"
             + " 4 1 roll abs 0.0001 lt  4 1 roll abs 0.0001 lt"
             + " 4 1 roll abs 0.0001 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 4));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest26Currentcmykcolor() throws Exception {
+    public void currentcmykcolor2() throws Exception {
         String cmd = "0.25 1 0.5 setrgbcolor currentcmykcolor"
             + " 0 sub abs 1e-4 lt"
             + " 4 1 roll 0.5 sub abs 1e-4 lt"
@@ -290,37 +290,37 @@ public class GraphicsPSTest {
             + " 4 1 roll 0.0 sub abs 1e-4 lt"
             + " 4 1 roll 0.33333 sub abs 1e-4 lt"
             + " 4 1 roll 0.66666 sub abs 1e-4 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 8));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest27Currentcmykcolor() throws Exception {
+    public void currentcmykcolor3() throws Exception {
         String cmd = "0.1 0.2 0.3 0.4 setcmykcolor currentcmykcolor 0.4 sub abs"
             + " 0.0001 lt  4 1 roll 0.3 sub abs 0.0001 lt  4 1 roll 0.2 sub abs"
             + " 0.0001 lt  4 1 roll 0.1 sub abs 0.0001 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 4));
     }
 
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest28Currentlinewidth() throws Exception {
+    public void currentlinewidth1() throws Exception {
         String cmd = "currentlinewidth 1 eq"
             + " 2 setlinewidth 0.5 0.5 scale currentlinewidth 2 eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
     
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest29() throws Exception {
+    public void strokeadjust1() throws Exception {
         String cmd = "true setstrokeadjust currentstrokeadjust"
             + " false setstrokeadjust currentstrokeadjust false eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 2));
     }
     
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest30() throws Exception {
+    public void misc1() throws Exception {
         String cmd = "currentcolorrendering type /dicttype eq"
             + " currentoverprint false eq"
             
@@ -337,12 +337,12 @@ public class GraphicsPSTest {
             + " currentflat 1.0 sub abs 1e-5 lt"
             + " currentsmoothness type /realtype eq";
 
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 8));
     }
     
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest31() throws Exception {
+    public void misc2() throws Exception {
         String cmd = "currentcolorrendering setcolorrendering true"
             + " true setoverprint currentoverprint"
             + " {} dup setblackgeneration currentblackgeneration eq"
@@ -351,12 +351,12 @@ public class GraphicsPSTest {
             + " currenthalftone sethalftone true"
             + " 0.398 setflat currentflat 0.398 sub abs 1e-5 lt"
             + " 0.0783 setsmoothness currentsmoothness 0.0783 sub abs 1e-5 lt";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 8));
     }
     
     /** Test. @throws Exception the exception */
     @Test
-    public void graphicsTest32() throws Exception {
+    public void misc3() throws Exception {
         String cmd = "{} dup settransfer currentcolortransfer pop pop pop eq"
             + " /redproc {/red pop} dup 3 1 roll def"
             + " /greenproc {/green pop} dup 3 1 roll def"
@@ -367,7 +367,7 @@ public class GraphicsPSTest {
             + " 4 1 roll userdict /blueproc get eq"
             + " 4 1 roll userdict /greenproc get eq"
             + " 4 1 roll userdict /redproc get eq";
-        assertTrue(Common.testString(interp, cmd));
+        assertTrue(Common.testString(interp, cmd, 5));
     }
     
 
